f0634b87847adfa5cd7b276d5f15a43acad818e1
Gayane25 <gayabennu@gmail.com>
2021-12-07 21:56:58 +0400

    written with loop and condition

--- isPolindrome.js
+++ isPolindrome.js
@@ -1,2 +1,13 @@
 //Check whether string is palindrome, or not.
 'use strict';
+
+const palindrome = str => {
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return 'no palindrome';
+    }
+    return 'palindrome';
+  }
+};
+
+console.log(palindrome('level'));
