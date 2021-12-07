f6df0f427d566a37bfcf985001fb606ec47934d2
Gayane25 <gayabennu@gmail.com>
2021-12-07 17:34:36 +0400

    solved with callback function

--- redundantStr.js
+++ redundantStr.js
@@ -1,2 +1,15 @@
 'use strict';
 //Write a function redundant that takes in a string str and returns a function that returns str.
+const redundant = function (str) {
+  return function () {
+    return str;
+  };
+};
+redundant()();
+
+const f1 = redundant('apple');
+console.log(f1());
+const f2 = redundant('pear');
+console.log(f2());
+const f3 = redundant('');
+console.log(f3());
