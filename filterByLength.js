b9c4bc33b1f2ee838ecc24104fab852f383bb44b
Gayane25 <gayabennu@gmail.com>
2021-12-07 19:31:01 +0400

    solved with loop and conditional operation

--- filterByLength.js
+++ filterByLength.js
@@ -1,2 +1,14 @@
 'use strict';
 //Write a function which remove elements with length <= 3
+
+function filterByLength(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length - 1 >= 3) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+console.log(filterByLength(['kia', 'tesla', 'bmw', 'mercedes']));
