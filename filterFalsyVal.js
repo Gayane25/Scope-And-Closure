ba7f8a38760a568ac1bc2afd6821cbff870cb8a4
Gayane25 <gayabennu@gmail.com>
2021-12-07 18:55:56 +0400

    solved with loop and candition operator

--- filterFalsyVal.js
+++ filterFalsyVal.js
@@ -1,2 +1,15 @@
 'use strict';
 //Write a function that implements filtering in array
+//const values = [null, true, {}, { name: "Elon" }, "", NaN, 0];
+
+function filterFalsyValues(values) {
+  let filterArray = [];
+  for (let i = 0; i < values.length; i++) {
+    if (values[i]) {
+      filterArray.push(values[i]);
+    }
+  }
+  return filterArray;
+}
+
+console.log(filterFalsyValues([null, true, {}, { name: 'Elon' }, '', NaN, 0]));
