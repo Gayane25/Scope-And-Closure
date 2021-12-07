909d5dda57e0b1a728776968efa190635c6a9583
Gayane25 <gayabennu@gmail.com>
2021-12-07 18:24:59 +0400

    solved with function returning anonymous function

--- addSuffix.js
+++ addSuffix.js
@@ -1,3 +1,17 @@
 'use strict';
 /*Write a function that returns an anonymous function, which transforms its input by adding a
 particular suffix at the end.*/
+const add_suffix = function (suffix) {
+  return function (some) {
+    return some + suffix;
+  };
+};
+add_suffix()();
+
+const add_ly = add_suffix('ly');
+console.log(add_ly('hopeless'));
+console.log(add_ly('total'));
+
+const add_less = add_suffix('less');
+console.log(add_less('fear'));
+console.log(add_less('ruth'));
