682363feb78364e6f3df36a698175b09ac891ec4
Gayane25 <gayabennu@gmail.com>
2021-12-07 17:11:57 +0400

    added a 'use strict' and a function

--- partialSum.js
+++ partialSum.js
@@ -1,1 +1,16 @@
 //Create a function that implements partial sum
+'use strict';
+
+const sum = function (operand) {
+  return function (operand2) {
+    return operand + operand2;
+  };
+};
+console.log(sum(1)(2));
+
+const addOne = sum(1);
+console.log(addOne(2));
+const addTen = sum(10);
+console.log(addTen(2));
+console.log(addOne(4));
+console.log(addTen(10));
