b6fa5166438045aa5e9069f7de552d9feaa57b3e
Gayane25 <gayabennu@gmail.com>
2021-12-07 20:50:29 +0400

    solved with indexOf and filter methods

--- uniqueVals.js
+++ uniqueVals.js
@@ -1,2 +1,9 @@
 'use strict';
 //Given an array. Determine whether it consists only from uniques or not.
+const unique = (value, index, self) => {
+  return self.indexOf(value) === index;
+};
+const nums = [26, 27, 26, 26, 28, 28, 29, 29, 30];
+const uniqueVals = nums.filter(unique);
+
+console.log(uniqueVals);
